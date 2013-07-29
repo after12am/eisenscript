bb6d8dfe86a7915b69c60b3d289325420b04708f
after12am <me.after.12am@gmail.com>
2013-07-30 02:20:48 +0900

    cleanup

--- src/context.js
+++ src/context.js
@@ -1,22 +1,22 @@
 var Context = function(code, option) {
-  this.seed;
   this.code = code;
   this.define = [];
   this.rules = {};
   this.computed = [];
-  this.parse();
+  this.create();
 }
 
-Context.prototype.parse = function() {
+Context.prototype.create = function() {
   var that = this;
   this.code.forEach(function(statement) {
     switch (statement.type) {
-      case Syntax.Define: that.define.push(statement); break;
-      case Syntax.Set: that.define.push(statement); break;
-      case Syntax.Statement: if (statement.computed) that.computed.push(statement); break;
-      case Syntax.Rule: that.register(statement); break;
-      default: console.error(statement); throw 'Unexpected Statement Error';
+    case Syntax.Define: that.define.push(statement); return;
+    case Syntax.Set: that.define.push(statement); return;
+    case Syntax.Statement: if (statement.computed) that.computed.push(statement); return;
+    case Syntax.Rule: that.register(statement); return;
     }
+    console.error(statement);
+    throw 'Unexpected Statement Error';
   });
 }
 
@@ -27,17 +27,16 @@ Context.prototype.register = function(rule) {
   rule.params.forEach(function(param) {
     if (param.type === Syntax.Modifier) {
       switch (param.key) {
-        case 'weight':
-          rule.weight = param.value;
-          break;
-        case 'maxdepth':
-          rule.maxdepth = param.value;
-          rule.alternate = param.alternate;
-          break;
-        default:
-          console.error(param);
-          throw 'Unexpected Modifier Error';
+      case 'weight':
+        rule.weight = param.value;
+        return;
+      case 'maxdepth':
+        rule.maxdepth = param.value;
+        rule.alternate = param.alternate;
+        return;
       }
+      console.error(param);
+      throw 'Unexpected Modifier Error';
     }
   });
   delete rule.params;
