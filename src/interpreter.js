66ecff6f1c0cef46cc4cef840d859beb6a3f9122
after12am <me.after.12am@gmail.com>
2013-07-30 23:21:00 +0900

    add comment to interpreter.js

--- src/interpreter.js
+++ src/interpreter.js
@@ -6,20 +6,22 @@ var Interpreter = function(ast, option) {
   this.maxobjects = 1000;
   this.minsize = .2;
   this.maxsize = 1.0;
-  this.seed = 1; // integer or inital
+  this.seed = 1; // integer or initial
 }
 
 Interpreter.prototype.interpret = function() {
   var that = this, define = [], rules = {}, computed = [];
+  // create intermediate code in order to facilitate the scrutiny
   this.ast.forEach(function(statement) {
     switch (statement.type) {
-      case Symbol.Define: define.push(statement); return;
-      case Symbol.Set: define.push(statement); return;
-      case Symbol.Statement: if (statement.computed) computed.push(statement); return;
-      case Symbol.Rule: rules = that.rewriteRule(rules, statement); return;
+      case Symbol.Define: define.push(statement); break;
+      case Symbol.Set: define.push(statement); break;
+      case Symbol.Statement: if (statement.computed) computed.push(statement); break;
+      case Symbol.Rule: rules = that.rewrite(rules, statement); break;
       default: throw 'Unexpected Statement Error';
     }
   });
+  // here is the intermediate code
   return {
     define: define,
     rules: rules,
@@ -27,18 +29,16 @@ Interpreter.prototype.interpret = function() {
   };
 }
 
-// add rule to rules stack object
-Interpreter.prototype.rewriteRule = function(rules, rule) {
+// rewrite subtree of rules
+Interpreter.prototype.rewrite = function(rules, rule) {
+  // defined the property if not defined
   if (!rules[rule.id]) rules[rule.id] = [];
-  // rewrite abstract syntax tree
+  // rewrite the subtree related to rule
   rule.params.forEach(function(param) {
     if (param.type === Symbol.Modifier) {
       switch (param.key) {
-        case Modifier.Weight: rule.weight = param.value; return;
-        case Modifier.Maxdepth:
-          rule.maxdepth = param.value;
-          rule.alternate = param.alternate;
-          return;
+        case Modifier.Weight: rule.weight = param.value; break;
+        case Modifier.Maxdepth: rule.maxdepth = param.value; rule.alternate = param.alternate; break;
         default: throw 'Unexpected Modifier Error';
       }
     }
@@ -57,10 +57,12 @@ Interpreter.prototype.run = function() {
     switch (statement.type) {
       case Symbol.Set:
         switch (statement.key) {
+          // set termination criteria
           case Modifier.Maxdepth: that.maxdepth = statement.value; break;
           case Modifier.Maxobjects: that.maxobjects = statement.value; break;
           case Modifier.Minsize: that.minsize = statement.value; break;
           case Modifier.Maxsize: that.maxsize = statement.value; break;
+          // other promise
           case Modifier.Seed: that.seed = statement.value; break;
           case Modifier.Background: that.renderer.setBackground(statement.value); break;
         }
