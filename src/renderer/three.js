992e566ba54dfc2710006c8ceb98763e32d4f5f6
after12am <me.after.12am@gmail.com>
2013-08-03 21:02:12 +0900

    fix indent

--- src/renderer/three.js
+++ src/renderer/three.js
@@ -1,34 +1,34 @@
 var ThreeRenderer = function(context, option) {
   
   var container = document.createElement('div');
-	document.body.appendChild(container);
-	
-	this.renderer = new THREE.CanvasRenderer();
-	this.scene = new THREE.Scene();
-	
-	var pos = 20;
-	this.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 2000);
-	this.camera.position.set(pos, pos, pos);
+  document.body.appendChild(container);
+  
+  this.renderer = new THREE.CanvasRenderer();
+  this.scene = new THREE.Scene();
+  
+  var pos = 20;
+  this.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 2000);
+  this.camera.position.set(pos, pos, pos);
   this.camera.lookAt( this.scene.position );
   
-	
-	// Grid
   
-	var size = 500, step = 10;
+  // Grid
   
-	var geometry = new THREE.Geometry();
+  var size = 1000, step = 10;
   
-	for ( var i = - size; i <= size; i += step ) {
-		geometry.vertices.push( new THREE.Vector3( - size, - 120, i ) );
-		geometry.vertices.push( new THREE.Vector3(   size, - 120, i ) );
-		geometry.vertices.push( new THREE.Vector3( i, - 120, - size ) );
-		geometry.vertices.push( new THREE.Vector3( i, - 120,   size ) );
-	}
+  var geometry = new THREE.Geometry();
   
-	var material = new THREE.LineBasicMaterial( { color: 0xffffff, opacity: 0.2 } );
-	var line = new THREE.Line(geometry, material);
-	line.type = THREE.LinePieces;
-	this.scene.add( line );
+  for ( var i = - size; i <= size; i += step ) {
+    geometry.vertices.push( new THREE.Vector3( - size, - 100, i ) );
+    geometry.vertices.push( new THREE.Vector3(   size, - 100, i ) );
+    geometry.vertices.push( new THREE.Vector3( i, - 100, - size ) );
+    geometry.vertices.push( new THREE.Vector3( i, - 100,   size ) );
+  }
+  
+  var material = new THREE.LineBasicMaterial( { color: 0xffffff, opacity: 0.2 } );
+  var line = new THREE.Line(geometry, material);
+  line.type = THREE.LinePieces;
+  this.scene.add( line );
   
   
   // CREATE OBJECT
@@ -42,8 +42,8 @@ var ThreeRenderer = function(context, option) {
   }
   
   
-	this.renderer.setSize(window.innerWidth, window.innerHeight);
-	container.appendChild(this.renderer.domElement);
+  this.renderer.setSize(window.innerWidth, window.innerHeight);
+  container.appendChild(this.renderer.domElement);
 }
 
 ThreeRenderer.prototype.setBackground = function(hex, alpha) {
@@ -66,9 +66,9 @@ ThreeRenderer.prototype.setPrimitive = function(primitive) {
   );
   box.applyMatrix(primitive.matrix);
   
-	this.scene.add( box );
+  this.scene.add( box );
 }
 
 ThreeRenderer.prototype.render = function() {
-	this.renderer.render(this.scene, this.camera);
+  this.renderer.render(this.scene, this.camera);
 }
