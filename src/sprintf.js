cc9642511ab5eb00b8ec20d86cb0655ab2ab9699
after12am <satosi.okami@gmail.com>
2016-07-02 15:23:14 +0900

    fix #31

--- src/sprintf.js
+++ src/sprintf.js
@@ -1,5 +1,18 @@
+function makeArray(array) {
+	var ret = [];
+	if(array != null){
+		var i = array.length;
+		if ( i == null || array.split || array.setInterval || array.call ) {
+      ret[0] = array;
+    } else {
+      while( i ) ret[--i] = array[i];
+    }
+	}
+	return ret;
+}
+
 function sprintf(){
-  var _arg = $.makeArray(arguments), template = _arg.shift(), i;
+  var _arg = makeArray(arguments), template = _arg.shift(), i;
   for(i in _arg){
     template = template.replace('%s', _arg[i]);
   }
