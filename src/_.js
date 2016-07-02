464d06db0ae619adcc8d5f913e0b50a8634eb49c
after12am <satosi.okami@gmail.com>
2016-07-02 19:18:22 +0900

    bump up v1.1.4 #36

---
+++ src/_.js
@@ -0,0 +1,16 @@
+var _ = {
+  has: function(obj, key) {
+    return hasOwnProperty.call(obj, key);
+  },
+
+  extend: function(destination, source) {
+    for(var k in source) destination[k] = source[k];
+    return destination;
+  },
+
+  values: function(obj) {
+    var values = [];
+    for (var key in obj) if (this.has(obj, key)) values.push(obj[key]);
+    return values;
+  }
+};
