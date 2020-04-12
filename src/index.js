a06a92c46dd11112705fbd0e5f09ed76a8b67142
after12am <after12am@users.noreply.github.com>
2020-04-12 16:16:05 +0900

    v2.0.0 kick out TestRenderer from bundle file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import Compiler from './Compiler';
 import OBJExporter from './exporter/OBJExporter';
-import TestRenderer from './test/TestRenderer';
 
 module.exports.Compiler = Compiler;
 module.exports.OBJExporter = OBJExporter;
-module.exports.TestRenderer = TestRenderer;
